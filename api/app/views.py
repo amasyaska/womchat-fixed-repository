3946e3eb0b6ebad234f0892299414280a6cafe79
CrazyDuck192 <pavelpuzanov35@gmail.com>
2024-04-30 11:46:47 +0300

    add error messages in responses

--- api/app/views.py
+++ api/app/views.py
@@ -38,7 +38,10 @@ class UserRegisterView(CreateAPIView):
                 return Response(
                     status=status.HTTP_200_OK
                 )
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            data={'error': 'Invalid data.'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
     
 
 class UserAuthenticateView(APIView):
@@ -50,10 +53,17 @@ class UserAuthenticateView(APIView):
         if serializer.is_valid(raise_exception=True):
             user = authenticate(request, **serializer.validated_data)
             if user is None:
-                return Response(status=status.HTTP_401_UNAUTHORIZED)
+                return Response(
+                    data={
+                        "error": "User with this account doesn't exist or your account was disabled."
+                    }, status=status.HTTP_401_UNAUTHORIZED
+                )
             login(request, user)
             return Response(status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            data={"error": "Invalid data."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
     
 
 class UserLogoutView(APIView):
