279c3d870f1c95fc09960dcdc7281a36b7c8693d
CrazyDuck192 <pavelpuzanov35@gmail.com>
2024-04-30 11:20:14 +0300

    changes in settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -22,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_config("SECRET_KEY")
+SECRET_KEY = env_config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env_config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '0.0.0.0',
+]
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost',
